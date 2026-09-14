7fce758a4652b3c70b76c4a3bb64c8af0cc87685
Meezan Faisal <meezanfaisal786@gmail.com>
2026-09-15 04:26:28 +0530

    Add Project to github

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -48,7 +47,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   // crypto: {
-  //   secret: "mysupersecretcodemustbeatleast32characterslong",
+  //   secret: process.env.SECRET,
   // },
   touchAfter: 24 * 3600,
 });
@@ -59,7 +58,7 @@ store.on("error", (err) => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcodemustbeatleast32characterslong",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
